b58816830e2ea07d306e7de53935a23dc2446029
Mateus Almeida <mateusalm012@gmail.com>
2025-09-21 18:36:29 -0300

    feat: add card button component

---
+++ frontend/src/components/CardButton.tsx
@@ -0,0 +1,29 @@
+﻿import { ButtonHTMLAttributes, CSSProperties } from 'react'
+
+const baseStyle: CSSProperties = {
+  display: 'block',
+  width: '100%',
+  textAlign: 'left',
+  cursor: 'pointer'
+}
+
+type Props = ButtonHTMLAttributes<HTMLButtonElement>
+
+export default function CardButton({
+  className = '',
+  style,
+  type,
+  children,
+  ...rest
+}: Props) {
+  return (
+    <button
+      type={type ?? 'button'}
+      className={`card${className ? ` ${className}` : ''}`}
+      style={{ ...baseStyle, ...style }}
+      {...rest}
+    >
+      {children}
+    </button>
+  )
+}
